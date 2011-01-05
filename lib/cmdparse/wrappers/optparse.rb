add1f9cf32c809982de3fca621d54f7d8a00b002
Thomas Leitner <t_leitner@gmx.at>
2011-01-05 20:56:01 +0100

    Changed license to LGPLv3

--- lib/cmdparse/wrappers/optparse.rb
+++ lib/cmdparse/wrappers/optparse.rb
@@ -1,24 +1,22 @@
 #
 #--
-#
-# $Id$
-#
 # cmdparse: advanced command line parser supporting commands
-# Copyright (C) 2004 Thomas Leitner
+# Copyright (C) 2004-2010 Thomas Leitner
 #
-# This program is free software; you can redistribute it and/or modify it under the terms of the GNU
-# General Public License as published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
+# This file is part of cmdparse.
 #
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# cmdparse is free software: you can redistribute it and/or modify it under the terms of the GNU
+# Lesser General Public License as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# cmdparse is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
 # General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along with this program; if not,
-# write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+# You should have received a copy of the GNU Lesser General Public License along with cmdparse. If
+# not, see <http://www.gnu.org/licenses/>.
 #
 #++
-#
 #
 
 require 'optparse'
